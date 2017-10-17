b2bf9f62874b55daeeb5e922ca15649ea75ab2ed
Luis Eduardo Estrada <internetgdl@MacBook-Air-de-Luis.local>
2017-10-16 20:51:14 -0500

    HOlas

--- Suck_Company/web_app/models.py
+++ Suck_Company/web_app/models.py
@@ -5,6 +5,11 @@ class Company(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=400)
 
-
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+class Comment(models.Model):
+    company =  Company
+    category = Category
+    rate = models.IntegerField
+    comments = models.CharField(max_length=400)
